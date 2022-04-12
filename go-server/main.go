6d641e3a3cca8dd74f5d1ef8fafa9dc2e85e2977
Leo Borquez <leoborquezg@protonmail.com>
2022-04-11 23:45:48 -0400

    update mac

--- go-server/main.go
+++ go-server/main.go
@@ -17,6 +17,7 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Done")
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
